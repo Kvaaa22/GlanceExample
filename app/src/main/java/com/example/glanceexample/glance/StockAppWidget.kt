cd415f1a098916252786d6331d60f69ece714022
Kvaaa22 <kvaaaa5a@gmail.com>
2025-12-11 16:16:14 +0700

    Моделирование изменения цен

--- app/src/main/java/com/example/glanceexample/glance/StockAppWidget.kt
+++ app/src/main/java/com/example/glanceexample/glance/StockAppWidget.kt
@@ -8,19 +8,45 @@ import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
+import androidx.glance.appwidget.updateAll
 import androidx.glance.background
 import androidx.glance.layout.Column
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.padding
-
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.time.Duration.Companion.seconds
 class StockAppWidget: GlanceAppWidget()  {
+    private var job: Job? = null
     override suspend fun provideGlance(context: Context, id: GlanceId) {
+        if (job == null) {
+            job = startUpdateJob(
+                20.seconds.inWholeMilliseconds,
+                context
+            )
+        }
         provideContent {
             GlanceTheme {
                 GlanceContent()
             }
         }
+
+
     }
+
+    private fun startUpdateJob(timeInterval: Long, context: Context): Job {
+        return CoroutineScope(Dispatchers.Default).launch {
+            while (true) {
+                PriceDataRepo.update()
+                StockAppWidget().updateAll(context)
+                delay(timeInterval)
+            }
+        }
+    }
+
     @Composable
     fun GlanceContent() {
         Column(modifier = GlanceModifier
